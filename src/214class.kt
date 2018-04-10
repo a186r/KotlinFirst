d0df15a5bbd63f8a603d58c96ec8c83de2eb5b9c
a186r <hi.aewalle@gmail.com>
2018-04-10 23:23:25 +0800

    214kt

---
+++ src/214class.kt
@@ -0,0 +1,52 @@
+/*
+fun main(args : Array<String>){
+    val name = if(args.size>0) args[0] else "Kotlin"
+    println("Hello,$name!")
+
+    //还可以进行更复杂的表达式
+    if(args.size>0){
+        println("Hello,${args[0]}!")
+    }
+}*/
+
+/*
+fun main(args : Array<String>){
+    println("Hello,${if(args.size > 0) args[0] else "someone"} !")
+}*/
+
+//class Person(val name : String)
+
+/*
+class Person(s: String, b: Boolean) {
+    val name : String = ""
+    var isMarried:Boolean = false
+}
+
+var person = Person("Bob",true)
+*/
+
+/*
+class Rectangle(val height:Int,val width:Int){
+    val isSquare:Boolean
+        get() = height == width
+
+        */
+/*get(){
+            return height == width
+        }*//*
+
+}*/
+
+
+//Kotlin不区分导入的是类还是函数，允许导入任何种类的声明，顶层函数可以直接导入
+import java.util.Random
+
+class Rectangle(val height:Int,val width:Int){
+    val isSquare:Boolean
+        get() = height == width
+}
+
+fun createRandomRectangle():Rectangle{
+    val  random = Random()
+    return Rectangle(random.nextInt(),random.nextInt())
+}
